04199c6ddc8eba867f4267367f3bbde9de2f7a03
Peter Dobbs <dobbsp8@gmail.com>
2019-06-26 13:16:59 -0500

    trying to setup actual api query

--- App/helper-script.R
+++ App/helper-script.R
@@ -1,15 +1,14 @@
 #helper
 #working on API calls
-username <- "something"
-password <- "something"
 
-base <- "https://something.what"
-endpoint <- "something"
-detail <- "something"
+base <- "http://hackathon.siim.org/fhir/Patient/siimjoe"
 
 call <- paste(base,endpoint,"?","ticker","=", detail, sep="")
 
+
 get_patient <- GET(call1, authenticate(username,password, type = "basic")) 
+
+
 
 get_patient_json <- fromJSON(get_patient_text, flatten = TRUE) 
 
