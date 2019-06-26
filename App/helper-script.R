f29aeb9b3e89475f49ace4f83d80d2f712d31cff
Anna <annaaiuppa@gmail.com>
2019-06-26 13:26:14 -0600

    implement API call to FHIR; works with requirement for system variable

--- App/helper-script.R
+++ App/helper-script.R
@@ -1,16 +1,17 @@
 #helper
 #working on API calls
-username <- "something"
-password <- "something"
 
-base <- "https://something.what"
-endpoint <- "something"
-detail <- "something"
+require("httr")
+require("jsonlite")
 
-call <- paste(base,endpoint,"?","ticker","=", detail, sep="")
-
-get_patient <- GET(call1, authenticate(username,password, type = "basic")) 
-
+url <- "http://hackathon.siim.org/fhir/Patient/siimjoe"
+#dependency: local system variable called SiimApiKey
+myKey <-Sys.getenv('SiimApiKey')
+patientReturn <- GET(url, accept_json(), add_headers('apikey' = myKey))
+get_patient_text <- content(patientReturn, "text")
 get_patient_json <- fromJSON(get_patient_text, flatten = TRUE) 
+#view json object return
+get_patient_json
 
+#view table of returned data
 data <- as.data.frame(get_patient_json)
