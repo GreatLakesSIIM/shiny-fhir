629191d38340ff0e99ba9d2983da864e8ad65ba6
abarring <alexander.barrington@mu.edu>
2019-06-26 15:23:19 -0500

    Added funcitonal tab

--- App/app.R
+++ App/app.R
@@ -7,49 +7,68 @@ require("httr")
 require("jsonlite")
 
 labelMandatory <- function(label) {
-  tagList(
-    label,
-    span("*", class = "mandatory_star")
-  )
+  tagList(label,
+          span("*", class = "mandatory_star"))
 }
 
 #UI
 ui <- dashboardPage(
   dashboardHeader(title = "SIIM Excitement"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
-    )
-  ),
-  dashboardBody(
-    #tab items must correspond with values for tab names
+  dashboardSidebar(sidebarMenu(
+    menuItem(
+      "Dashboard",
+      tabName = "dashboard",
+      icon = icon("dashboard")
+    ),
+    menuItem("Patient", tabName = "patient", icon = icon("th"))
+  )),
+  dashboardBody(#tab items must correspond with values for tab names
     tabItems(
       #first tab
       tabItem(tabName = "dashboard",
               #boxes are put in rows or columns
               fluidRow(
                 box(DT::dataTableOutput("patient"), title = "Patient Data")
-              )
-      ),
+              )),
       #2nd tab
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content"))
-      
-    )
-  )
+      tabItem(tabName = "patient",
+              fluidPage(title = "Patient form example",
+                        fluidRow(column(
+                          6,
+                          div(
+                            id = "form",
+                            
+                            textInput("name", labelMandatory("Name"), "Patient Name"),
+                            dateInput("dob", labelMandatory("DOB")),
+                            checkboxInput("isSmoker", "Is smoker?", FALSE),
+                            sliderInput("r_num_years", "Number of years using R", 0, 25, 2, ticks = FALSE),
+                            selectInput(
+                              "os_type",
+                              "Operating system used most frequently",
+                              c("",  "Windows", "Mac", "Linux")
+                            ),
+                            actionButton("submit", "Submit", class = "btn-primary"),
+                            
+                            shinyjs::hidden(span(id = "submit_msg", "Submitting..."),
+                                            div(id = "error",
+                                                div(
+                                                  br(), tags$b("Error: "), span(id = "error_msg")
+                                                ))))))))))
 )
 
-server <- function(input, output){
+server <- function(input, output) {
   url <- "http://hackathon.siim.org/fhir/Patient/siimjoe"
   #dependency: local system variable called SiimApiKey
-  myKey <-Sys.getenv('SiimApiKey')
-  patientReturn <- GET(url, accept_json(), add_headers('apikey' = myKey))
+  myKey <- Sys.getenv('SiimApiKey')
+  patientReturn <-
+    GET(url, accept_json(), add_headers('apikey' = myKey))
   get_patient_text <- content(patientReturn, "text")
-  get_patient_json <- fromJSON(get_patient_text, flatten = TRUE) 
+  get_patient_json <- fromJSON(get_patient_text, flatten = TRUE)
   #table of returned data
   patientData <- as.data.frame(get_patient_json)
-  output$patient <- DT::renderDataTable({ patientData })
+  output$patient <- DT::renderDataTable({
+    patientData
+  })
 }
 
 shinyApp(ui, server)
